3804d9a47ed7b8f2f7854981e7ee9ef8f096fe0a
Omair Gibreel <omirgibreel7@gmail.com>
2024-06-25 09:23:02 +0400

    Add project files.

---
+++ Client/Pages/Contact.cs
@@ -0,0 +1,10 @@
+﻿namespace testvcf7.Client.Pages
+{
+    public class Contact
+    {
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string ImageDataUrl { get; set; }
+    }
+}
